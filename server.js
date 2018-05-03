3428dc14ddb0865fa2ef523f67426638b450d24e
Sun Davey <sundavey@hotmail.com>
2018-05-03 14:31:35 -0700

    editted

--- server.js
+++ server.js
@@ -9,17 +9,11 @@ app.set('views', path.join(__dirname + "/views"));
 
 app.set('view engine', 'ejs');
 
-const server = app.listen(1337);
+const server = app.listen(8000);
 const io = require('socket.io')(server);
 var counter = 0;
 
 io.on('connection', function (socket) { //2
-
-    socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
-    socket.on('thankyou', function (data) { //7
-        console.log(data.msg); //8 (note: this log will be on your server's terminal)
-    });
-
     socket.on('count', function(data){
         counter++;
         socket.emit('broad', counter);
